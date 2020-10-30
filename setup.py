4f0f3e92fd4806afef8e48cbab5f56d70d32b60d
KarthikNarla <karthiknarla22@gmail.com>
2020-10-30 10:17:57 +0100

    Changing version according to the error in conda CMD

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ setup(
     python_requires='>=3.6',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     install_requires=[
-        'gensim == 3.8.1',
+        'gensim == 3.8.3',
         'joblib == 0.14.0',
         'Keras-Preprocessing == 1.1.0',
         'nltk == 3.4.5',
         'numpy == 1.17.3',
         'pandas == 0.25.2',
-        'scikit-learn == 0.21.3',
-        'scipy == 1.5.2',
+        'scikit-learn == 0.23.2',
+        'scipy == 1.4.1',
     ],
     extras_require={
         'test': [
